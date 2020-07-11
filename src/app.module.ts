cf5801ae153c55e56abfa9532ba76c643413ff1e
Jose Ramon Baños Atalaya <50395211+jrbanos@users.noreply.github.com>
2020-07-11 02:24:38 +0200

    a

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [RecetaModule,
-    MongooseModule.forRoot('mongodb://jbatalaya.site:27020/recetasV3')],
+    MongooseModule.forRoot('mongodb://jbatalaya.site:27017/recetasV3')],
   controllers: [AppController],
   providers: [AppService],
 })
